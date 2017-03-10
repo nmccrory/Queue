cfcc925552bacd37ff31b4ad938e41302d18a13f
Nick McCrory <n.mccrory@icloud.com>
2017-03-09 19:42:08 -0800

    Created Queue class with most of the basic methods of a queque. Still missing the remove method.

---
+++ Queue.java
@@ -0,0 +1,39 @@
+/**
+ * Author: Nick McCrory
+ * Date: 3/9/2016
+ */
+
+public class Queue {
+    private Node first = null;
+    private int count = 0;
+
+    private class Node {
+        int data;
+        Node next = null;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    // Construct an empty Queue.
+    public Queue(){};
+    // Construct a Queue with a starting Node.
+    public Queue(int data) {
+        this.add(data);
+    }
+
+    public void add(int data) {
+        Node n = new Node(data);
+
+        if (this.first != null) {
+            n.next = this.first;
+        }
+        this.first = n;
+        this.count++;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+}
